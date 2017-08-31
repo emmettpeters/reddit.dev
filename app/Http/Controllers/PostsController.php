618699b6a67740575c608689afa2c03836c4da0b
Emmett Peters <ejp8611@gmail.com>
2017-08-31 16:42:23 -0500

    fixed functionality of the only one vote per post per user

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -132,8 +132,10 @@ class PostsController extends Controller
     {
         $post = Post::find($id);
         $user_id = \Auth::id();
-        $post_id = Post::find($id)->user_id;
-        if(empty(Vote::where('id',$id)->where('user_id',$user_id)->get())){
+        $post_id = Post::find($id)->id;
+        // dd($user_id);
+        $currVotes = Vote::where('post_id',$id)->where('user_id',$user_id)->get();
+        if($currVotes->isEmpty()){
             $vote = new Vote;
             $vote->user_id = $user_id;
             $vote->post_id = $post_id;
@@ -149,12 +151,15 @@ class PostsController extends Controller
     {
         $post = Post::find($id);
         $user_id = \Auth::id();
-        $post_id = Post::find($id)->user_id;
-        $vote = new Vote;
-        $vote->user_id = $user_id;
-        $vote->post_id = $post_id;
-        $vote->vote = -1;
-        $vote->save();
+        $post_id = Post::find($id)->id;
+        $currVotes = Vote::where('post_id',$id)->where('user_id',$user_id)->get();
+        if($currVotes->isEmpty()){
+            $vote = new Vote;
+            $vote->user_id = $user_id;
+            $vote->post_id = $post_id;
+            $vote->vote = -1;
+            $vote->save();
+        };
 
         $data['post'] = $post;
         return view('posts.show',$data);
